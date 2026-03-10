044f7a5c13dcd02c529bb57392a80ecaa0e7be26
AMiyazono <allan.miyazono@gmail.com>
2026-03-10 13:22:18 -0600

    Improved parse arguments

--- pipeline.sh
+++ pipeline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 var_path=$PWD  
-python preprocess_mi.py --input data/example.tsv --output data/simplified.csv
+python preprocess_mi.py --input data/example.tsv --output data/simplified.csv --separation "t" --axis 1
 Rscript R_discretize.R --input data/simplified.csv --output data/discretized.csv --working_dir $var_path
 python generate_matrix.py --input data/discretized.csv --output data/mi_matrix.csv
